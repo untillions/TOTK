f12b911ff59531d6e7d658c1b0649595c13add23
untillions <untillions@gmail.com>
2026-07-02 19:24:53 -0500

    update files

---
+++ landmarks.js
@@ -0,0 +1,24 @@
+// REGION ID REFERENCE:
+// 1: North Hyrule Field     6: Great Hyrule Forest      11: Mount Lanayru
+// 2: South Hyrule Field     7: Eldin                    12: East Necluda
+// 3: Hyrule Ridge           8: Akkala                   13: Faron Grasslands
+// 4: Hebra Mountains        9: Lanayru Great Spring     14: Gerudo Desert
+// 5: Tabantha Tundra       10: Lanayru Wetlands         15: Gerudo Highlands
+
+const landmarks = [
+    { name: "Lookout Landing", type: "Tower", region: 1, level: "Surface", x: -298, y: 142, z: 25, details: "" },
+    { name: "Hyrule Field", type: "Tower", region: 2, level: "Surface", x: -761, y: -1019, z: 64, details: "" },
+    { name: "Lindor's Brow", type: "Tower", region: 3, level: "Surface", x: -1909, y: 1245, z: 297, details: "" },
+    { name: "Rospro Pass", type: "Tower", region: 4, level: "Surface", x: -3860, y: 2345, z: 233, details: "" },
+    { name: "Pikida Stonegrove", type: "Tower", region: 5, level: "Surface", x: -2312, y: 3064, z: 536, details: "" },
+    { name: "Typhlo Ruins", type: "Tower", region: 6, level: "Surface", x: 343, y: 3133, z: 180, details: "" },
+    { name: "Eldin Canyon", type: "Tower", region: 7, level: "Surface", x: 1641, y: 1190, z: 225, details: "" },
+    { name: "Ulri Mountain", type: "Tower", region: 8, level: "Surface", x: 3497, y: 2025, z: 188, details: "" },
+    { name: "Upland Zorana", type: "Tower", region: 9, level: "Surface", x: 2865, y: 580, z: 379, details: "" },
+    { name: "Sahasra Slope", type: "Tower", region: 10, level: "Surface", x: 1341, y: -1176, z: 166, details: "" },
+    { name: "Mount Lanayru", type: "Tower", region: 11, level: "Surface", x: 3847, y: -1313, z: 539, details: "" },
+    { name: "Rabella Wetlands", type: "Tower", region: 12, level: "Surface", x: 2419, y: -2755, z: 222, details: "" },
+    { name: "Popla Foothills", type: "Tower", region: 13, level: "Surface", x: 604, y: -2127, z: 98, details: "" },
+    { name: "Gerudo Canyon", type: "Tower", region: 14, level: "Surface", x: -2438, y: -2182, z: 307, details: "" },
+    { name: "Gerudo Highlands", type: "Tower", region: 15, level: "Surface", x: -3959, y: -1313, z: 422, details: "" }
+];
